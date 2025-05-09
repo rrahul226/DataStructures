4b919f3a6568f6336588c3c55599a918c1cb090a
rrahul226 <165047126+rrahul226@users.noreply.github.com>
2025-05-09 20:48:02 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int N=sc.nextInt();
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("*************************************************\n");
+        for(int i=1;i<N+1;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        System.out.println("************************************************\n");
+        for(int i=1;i<N+1;i++)
+        {
+            for(int k=N;k>i;k--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        System.out.println("**************************************************\n");
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=N;k>i;k--)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
