7b7ac4fc5f41db54dec978c9eb915c9a22615d6f
rrahul226 <165047126+rrahul226@users.noreply.github.com>
2025-05-09 11:16:45 +0530

    Add files via upload
    Using Cyclic Sort to find a duplicate number of the given array

---
+++ DuplicateUsingCyclic.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class DuplicateUsingCyclic {
+    public static void main(String[] args) {
+        System.out.println("Enter No.Of Elements to array: ");
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        System.out.println("Enter Elements: ");
+        int[] arr=new int[N];
+        for(int j=0;j<N;j++)
+        {
+            arr[j]=sc.nextInt();
+        }
+        int i=0;
+        while(i<arr.length)
+        {
+            int correct=arr[i]-1;
+            if(arr[i]<arr.length && arr[i]!=arr[correct])
+            {
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }
+            else    
+                i++;
+        }
+        System.out.print("Duplicate Number of the array: ");
+        for(int j=0;j<arr.length;j++)
+        {
+            if(arr[j]!=j+1)
+                System.out.println(arr[j]);
+        }
+    }
+}
